576718f997d130880dbc46872693d55ada81e622
leowangj <rfv7855659@gmail.com>
2023-07-11 21:55:19 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import TonConnector from "./components/Ton-Connector";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import TWA from '@twa-dev/sdk'
+import WebApp from '@twa-dev/sdk'
 const queryClient = new QueryClient();
 
 function App() {
-  console.log(TWA)
+  console.log(WebApp)
+  WebApp.showAlert('Hey there!');
+  WebApp.sendData(JSON.stringify({
+    is_visible: true,
+    test: 'I am leo'
+  }));
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
