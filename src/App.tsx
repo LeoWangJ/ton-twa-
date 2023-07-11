2be288e64244b4204b6fa86baa5f002f84dd488b
leowangj <leo2023chen@gmail.com>
2023-07-11 15:41:53 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import TonConnector from "./components/Ton-Connector";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import TWA from '@twa-dev/sdk'
 const queryClient = new QueryClient();
 
 function App() {
+  console.log(TWA)
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
